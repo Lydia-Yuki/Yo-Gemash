b7773d65c460d04c4849399668235aca60d92c3e
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-07-21 19:13:06 +0200

    added apo jesu anouya

--- lib/songs/apo_jesu_widget.dart
+++ lib/songs/apo_jesu_widget.dart
@@ -1,1 +1,35 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ApoJesuAnouya extends StatelessWidget {
+  const ApoJesuAnouya({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Apo Jesu Anouya"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Align(
+              alignment: const AlignmentDirectional(0, 0.15),
+              child: Text(
+                'Verse 1\nApo Jesu anouya kutora varume\nAnotora vakanaka avo vanomuda\n\nChorus\nVanhu vake vatsvene vasina zvitema\nVanoenda kudenga\nKunogara naJesu\n\nVerse 2\nApo Jesu anouya kutora vakadzi\nAnotora vakanaka avo vanomuda\n\nVerse 3\nApo Jesu anouya kutora vacheche\nAnotora vakanaka avo vanomuda\n\nVerse 4\nVana vake pasi pano uyai kwaari\nAnomuda zvino pano norudo rukuru\n\nVerse 5\nNdatiedze misi yose kufadza M\'ponesi\nTizotorwa ndiye Jesu kudenga kutsvene',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline2,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
